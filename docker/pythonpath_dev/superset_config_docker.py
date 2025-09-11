19c02d1509ab4fd020f60bd003df7a943f1fb3f2
Antoine Klein <antoine.klein@1spatial.com>
2025-09-11 13:17:07 +0000

    Add custom configuration

---
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -0,0 +1,20 @@
+#FEATURE_FLAGS = {
+#    'ENABLE_TEMPLATE_PROCESSING': True,
+#    'ESCAPE_MARKDOWN_HTML': False
+#}
+
+HTML_SANITIZATION = True
+HTML_SANITIZATION_SCHEMA_EXTENSIONS = {
+  "attributes": {
+    "*": ["style", "className", "class"],
+  },
+  "tagNames": ["style"],
+}
+
+# Translations
+BABEL_DEFAULT_LOCALE = 'fr'
+BABEL_DEFAULT_FOLDER = 'superset/translations'
+LANGUAGES = {
+    'en': {'flag': 'us', 'name': 'English'},
+    'fr': {'flag': 'fr', 'name': 'Français'},
+}
