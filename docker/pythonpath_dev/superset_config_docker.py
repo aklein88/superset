2cba4d80dfa512a438464ea412b5694b50210a58
Antoine Klein <antoine.klein@1spatial.com>
2025-10-02 10:03:41 +0000

    add talisman config

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -11,3 +11,22 @@ HTML_SANITIZATION_SCHEMA_EXTENSIONS = {
   "tagNames": ["style"],
 }
 
+GLOBAL_ASYNC_QUERIES = True
+GLOBAL_ASYNC_QUERIES_TRANSPORT = "ws"
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_NAME = "async-token"
+#GLOBAL_ASYNC_QUERIES_JWT_SECRET = "YOUR_CUSTOM_JWT_SECRET"
+#GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "ws://my.domain.com/ws" # use wss protocol (wss:) for SSL connection
+
+# TALISMAN_ENABLED = True
+# TALISMAN_CONFIG = {
+#     "content_security_policy": {
+#         "default-src": ["'self'"],
+#         "script-src": ["'self'", "https://trusted-scripts.com"],
+#         "img-src": ["'self'", "data:"],
+#         "connect-src": [
+#             "'self'",
+#             "ws://my.domain.com/ws",  # Add your websocket endpoint here, use wss protocol (wss:) for SSL connection
+#             "https://api.mapbox.com"
+#         ],
+#     },
+# }
