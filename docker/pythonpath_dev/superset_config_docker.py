68b6c0f802ded5609790c71f8a4569abbe4711e3
Antoine Klein <antoine.klein@1spatial.com>
2026-02-04 15:11:20 +0000

    Utilisation du fichier .env-local-websocket dans docker-compose-custom.yml

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -14,8 +14,10 @@ HTML_SANITIZATION_SCHEMA_EXTENSIONS = {
 GLOBAL_ASYNC_QUERIES = True
 GLOBAL_ASYNC_QUERIES_TRANSPORT = "ws"
 GLOBAL_ASYNC_QUERIES_JWT_COOKIE_NAME = "async-token"
-#GLOBAL_ASYNC_QUERIES_JWT_SECRET = "YOUR_CUSTOM_JWT_SECRET"
-#GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "ws://my.domain.com/ws" # use wss protocol (wss:) for SSL connection
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_SAMESITE=None
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_SECURE=True
+GLOBAL_ASYNC_QUERIES_JWT_SECRET = "453f8d0992c089699fb5d29c6f620984"
+GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "wss://rou-d-devops.1spatial.fr/ws" # use wss protocol (wss:) for SSL connection
 
 TALISMAN_ENABLED = True
 TALISMAN_CONFIG = {
@@ -25,7 +27,7 @@ TALISMAN_CONFIG = {
         "img-src": ["'self'", "data:"],
         "connect-src": [
             "'self'",
-            "ws://my.domain.com/ws",  # Add your websocket endpoint here, use wss protocol (wss:) for SSL connection
+            "wss://rou-d-devops.1spatial.fr/ws",  # Add your websocket endpoint here, use wss protocol (wss:) for SSL connection
             "https://api.mapbox.com"
         ],
     },
