2245778ffa9d143585767001d2da8d08099c7053
Antoine Klein <antoine.klein@1spatial.com>
2026-03-27 13:29:55 +0000

    Add Talisman config

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -19,16 +19,16 @@ GLOBAL_ASYNC_QUERIES_JWT_COOKIE_SECURE=True
 GLOBAL_ASYNC_QUERIES_JWT_SECRET = "453f8d0992c089699fb5d29c6f620984"
 GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "wss://rou-d-devops.1spatial.fr/ws" # use wss protocol (wss:) for SSL connection
 
-# TALISMAN_ENABLED = True
-# TALISMAN_CONFIG = {
-#     "content_security_policy": {
-#         "default-src": ["'self'"],
-#         "script-src": ["'self'", "https://trusted-scripts.com"],
-#         "img-src": ["'self'", "data:"],
-#         "connect-src": [
-#             "'self'",
-#             "ws://my.domain.com/ws",  # Add your websocket endpoint here, use wss protocol (wss:) for SSL connection
-#             "https://api.mapbox.com"
-#         ],
-#     },
-# }
+TALISMAN_ENABLED = True
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "default-src": ["'self'"],
+        "script-src": ["'self'", "https://trusted-scripts.com"],
+        "img-src": ["'self'", "data:"],
+        "connect-src": [
+            "'self'",
+            "wss://rou-d-devops.1spatial.fr/ws",  # Add your websocket endpoint here, use wss protocol (wss:) for SSL connection
+            "https://api.mapbox.com"
+        ],
+    },
+}
